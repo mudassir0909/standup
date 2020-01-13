06e6fd5c419ef2a34855e3c8e1567b25054e785c
Mudassir <mudassir.dev@gmail.com>
2020-01-13 15:27:14 +1100

    Update status_update.js

--- status_update.js
+++ status_update.js
@@ -17,5 +17,40 @@ if (files.length === 0) {
 const recentlyModified = files.map(f => path.join(STATUS_UPDATE_DIR, f)).sort((a, b) => {
   return fs.statSync(b).mtime - fs.statSync(a).mtime;
 })[0];
+const baseName = path.basename(recentlyModified);
 
-console.log(fs.readFileSync(recentlyModified).toString());
+let dd;
+let mm;
+let yyyy;
+try {
+  [dd, mm, yyyy] = validateFilename(baseName)
+} catch {
+  console.log('Invalid status update file found');
+  process.exit(1);
+}
+logStatusUpdate(fs.readFileSync(recentlyModified).toString(), { dd, mm, yyyy });
+
+function validateFilename(fileName) {
+  if (fileName.length !== "ddmmyyyy".length) {
+    throw new Error();
+  }
+  const [dd, mm, yyyy] = [
+    fileName.slice(0, 2),
+    fileName.slice(2, 4),
+    fileName.slice(4, 8),
+  ].map(x => parseInt(x, 10));
+
+  if ([dd, mm, yyyy].some(s => isNaN(s))) {
+    throw new Error();
+  }
+
+  return [dd, mm, yyyy];
+}
+
+function logStatusUpdate(update, { dd, mm, yyyy }) {
+  const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][mm];
+  const logs = update.split('\n').filter(x => !!x.trim()).map(x => `* ${x}`).join('\n');
+  const message = `Update for ${dd}-${month}-${yyyy}\n\n${logs}`
+
+  console.log(message);
+}
